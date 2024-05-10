ab7c18a4620caf40703d5b6fda0f65c5218a0e95
Sávio <115809910+savio27022022@users.noreply.github.com>
2024-05-09 22:41:00 -0300

    Create Teste-Dev-Exercicio.py

---
+++ Teste-Dev-Exercicio.py
@@ -0,0 +1,36 @@
+import os
+system.os("cls||clear");
+somaGeral=0
+quantidadeGeral=0
+somaPares=0
+pares=0
+impares=0
+while True:
+    numero=int(input("Digite um número:"))
+    if numero>0:
+        somaGeral+=numero
+        quantidadeGeral+=1
+
+        if numero %2==0:
+        somaPares+=numero
+        pares+=1
+        
+        else:
+        impares+=1
+        
+    if numero==0:
+        break
+    
+    mediaGeral=somaGeral/quantidadeGeral
+    
+    if pares!=0:
+        mediaPares=somaPares/pares
+    else:
+        mediaPares=0    
+    
+    print(===Exibindo Resultados====)
+    print(f"Quantidade  de números pares:{pares}")
+    print(f"Quantidade de números ímpares:{impares}")
+    print(f"Média pares:{mediaPares}")
+    print(f"MediaGeral{mediaGeral}")
+    
